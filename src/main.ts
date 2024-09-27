c120743d303ea14948e5bcc87704359061af0414
Aine-dickson <ainedixon01@gmail.com>
2024-09-27 20:35:27 +0300

    cors-reset

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useWebSocketAdapter(new IoAdapter(app));
-  app.use(cors({
-    origin: [/https:\/\/.*notipage\.netlify\.app$/], // Allow any subdomain of notipage.netlify.app
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization, set-cookie',
-    credentials: true,
-  }));
+  // app.use(cors({
+  //   origin: [/https:\/\/.*notipage\.netlify\.app$/], // Allow any subdomain of notipage.netlify.app
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   allowedHeaders: 'Content-Type, Authorization, set-cookie',
+  //   credentials: true,
+  // }));
   
-  // app.enableCors({
-  //   methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
-  //     allowedHeaders: 'Content-Type, Authorization, set-cookie',
-  //   credentials: true, // Allow credentials like cookies
-  // });
+  app.enableCors({
+    origin: [/https:\/\/.*notipage\.netlify\.app$/], // Allow any subdomain of notipage.netlify.app
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
+      allowedHeaders: 'Content-Type, Authorization, set-cookie',
+    credentials: true, // Allow credentials like cookies
+  });
 
   await app.listen(3000);
 }
